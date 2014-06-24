63bcdb3ec3938d0a011e0a62aeb3dae4a48dfe3c
Bazyl Reinstein <bazylrei@gmail.com>
2014-06-24 19:13:13 +0200

    classes added in CMakeLists, util class modified
    OpenGL math classes

--- include/cube/util.h
+++ include/cube/util.h
@@ -1,3 +1,5 @@
+#include <glm/glm.hpp>
+
 #ifndef UTIL_H
 #define UTIL_H
 
@@ -11,47 +13,20 @@ typedef signed char INT8;
 #define minmax(min_, x, max_)    (min(max(min_,x), max_))
 
 
-
-// remove, replace with glm::vec3
-class PT3D
-{
-public:
-   double x,y,z;
-public:
-   PT3D()                                    {};
-   PT3D(double xx, double yy, double zz)     {x=xx; y=yy; z=zz;};
-};
-
-
-// remove, replace with glm::vec2
-class PT2D 
-{
-public:
-   double x,y;
-public:
-   PT2D()                                    {};
-   PT2D(double xx, double yy)                {x=xx; y=yy;};
-   PT2D& operator =(const PT3D& pt)          {x=pt.x; y=pt.y; return *this;};
-};
-
-
-
-
-
 inline float toRads(float fDegrees)     {return fDegrees*2*PI/360;};
 inline float toDegs(float fRadians)     {return fRadians*360/(2*PI);};
 
 
-int lineTest(const PT2D& ptLineStart, const PT2D& ptLineEnd, const CPoint& pt);
+int lineTest(const glm::vec2& ptLineStart, const glm::vec2& ptLineEnd, const float ptX, const float ptY);
 // bool instead of BOOL
-BOOL poly4InsideTest(const PT2D* pt2Corners, const CPoint& pt);
-BOOL poly4InsideTest(PT3D* pt3Corners, const CPoint& pt);   
+bool poly4InsideTest(const glm::vec2 pt2Corners[], float ptX, float ptY);
+bool poly4InsideTest(glm::vec3 pt3Corners[], float ptX, float ptY);   
 
 // use const reference instead of *
 // (float x, float y) or glm::vec2 insteadd of CPoint
-INT8 getYsection(PT2D* ptCorners, const CPoint& pt);
-INT8 getYsection(PT3D* pt3Corners, const CPoint& pt);
-INT8 getXsection(PT2D* ptCorners, const CPoint& pt);
-INT8 getXsection(PT3D* pt3Corners, const CPoint& pt);
+int getYsection(glm::vec2 ptCorners[], float ptX, float ptY);
+int getYsection(glm::vec3 pt3Corners[], float ptX, float ptY);
+int getXsection(glm::vec2 ptCorners[], float ptX, float ptY);
+int getXsection(glm::vec3 pt3Corners[], float ptX, float ptY);
 
 #endif //UTIL_H
--- src/cube/util.cpp
+++ src/cube/util.cpp
@@ -1,73 +1,74 @@
-#include "stdafx.h"
 #include <math.h>
 
-#include "util.h"
+#include "cube/util.h"
 
 
 #define ALMOST_ZERO 1e-6     // error tolerance
 
-BOOL poly4InsideTest(const PT2D* ptCorners, const CPoint& pt)
+bool poly4InsideTest(const glm::vec2 ptCorners[], float ptX, float ptY)
 {
    int nResult1, nResult2;
 
-   nResult1 = lineTest(ptCorners[0], ptCorners[1], pt);
-   nResult2 = lineTest(ptCorners[2], ptCorners[3], pt);
+   nResult1 = lineTest(ptCorners[0], ptCorners[1], ptX, ptY);
+   nResult2 = lineTest(ptCorners[2], ptCorners[3], ptX, ptY);
 
-   if (nResult1 > 0 && nResult2 > 0 
-       || nResult1 < 0 && nResult2 < 0)
+   if (((nResult1 > 0) && (nResult2 > 0)) 
+       || ((nResult1 < 0) && (nResult2 < 0)))
    {
-      return FALSE;
+      return false;
    }
 
 
-   nResult1 = lineTest(ptCorners[0], ptCorners[3], pt);
-   nResult2 = lineTest(ptCorners[1], ptCorners[2], pt);
+   nResult1 = lineTest(ptCorners[0], ptCorners[3], ptX, ptY);
+   nResult2 = lineTest(ptCorners[1], ptCorners[2], ptX, ptY);
 
-   if (nResult1 > 0 && nResult2 > 0 
-       || nResult1 < 0 && nResult2 < 0)
+   if (((nResult1 > 0) && (nResult2 > 0)) 
+       || ((nResult1 < 0) && (nResult2 < 0)))
    {
-      return FALSE;
+      return false;
    }
 
 
-   return TRUE;
+   return true;
 }
 
-BOOL poly4InsideTest(PT3D* pt3Corners, const CPoint& pt)
+bool poly4InsideTest(const glm::vec3 pt3Corners[], float ptX, float ptY)
 {
-   PT2D pt2Corners[4];
+   glm::vec3 pt2Corners[4];
 
-   for (int i=0; i<4; i++) 
-      pt2Corners[i] = pt3Corners[i];
+   for (int i=0; i<4; i++) { 
+		pt2Corners[i].x = pt3Corners[i].x;
+		pt2Corners[i].y = pt3Corners[i].y;
+   }
 
-   return poly4InsideTest(pt2Corners, pt);
+   return poly4InsideTest(pt2Corners, ptX, ptY);
 }
 
-int lineTest(const PT2D& ptLineStart, const PT2D& ptLineEnd, const CPoint& pt)
+int lineTest(const glm::vec2& ptLineStart, const glm::vec2& ptLineEnd, float ptX, float ptY)
 {
    double dx = ptLineEnd.x - ptLineStart.x;
    double dy = ptLineEnd.y - ptLineStart.y;
 
 
    if (fabs(dx)>fabs(dy)) {
-      double yOnline = dy/dx * (pt.x - ptLineStart.x) + ptLineStart.y;
-      return pt.y >  yOnline 
+      double yOnline = dy/dx * (ptX - ptLineStart.x) + ptLineStart.y;
+      return ptY >  yOnline 
              ? 1
-             : (pt.y < yOnline ? -1 : 0);
+             : (ptY < yOnline ? -1 : 0);
    }
    else {
-      double xOnline = dx/dy * (pt.y - ptLineStart.y) + ptLineStart.x;
-      return pt.x >  xOnline 
+      double xOnline = dx/dy * (ptY - ptLineStart.y) + ptLineStart.x;
+      return ptX >  xOnline 
              ? 1
-             : (pt.x < xOnline ? -1 : 0);
+             : (ptX < xOnline ? -1 : 0);
    }
 }
 
 
-INT8 getYsection(PT2D* ptCorners, const CPoint& pt)
+int getYsection(glm::vec2 ptCorners[], float ptX, float ptY)
 {
    int rc0, rc1, rc2;
-   PT2D ptLineStart, ptLineEnd;
+   glm::vec2 ptLineStart, ptLineEnd;
    double dx, dy;
 
    dx = (ptCorners[2].x - ptCorners[1].x)/3;
@@ -79,22 +80,22 @@ INT8 getYsection(PT2D* ptCorners, const CPoint& pt)
    ptLineStart.y = ptCorners[0].y;
    ptLineEnd.x   = ptCorners[1].x;
    ptLineEnd.y   = ptCorners[1].y;
-   rc0 = lineTest(ptLineStart, ptLineEnd, pt);
+   rc0 = lineTest(ptLineStart, ptLineEnd, ptX, ptY);
 
    ptLineStart.x += dx;
    ptLineStart.y += dy;
    ptLineEnd.x   += dx;
    ptLineEnd.y   += dy;
-   rc1 = lineTest(ptLineStart, ptLineEnd, pt);
+   rc1 = lineTest(ptLineStart, ptLineEnd, ptX, ptY);
 
    ptLineStart.x += dx;
    ptLineStart.y += dy;
    ptLineEnd.x   += dx;
    ptLineEnd.y   += dy;
-   rc2 = lineTest(ptLineStart, ptLineEnd, pt);
+   rc2 = lineTest(ptLineStart, ptLineEnd, ptX, ptY);
 
-   if (rc1 >= 0 && rc2 <= 0 
-       || rc1 <= 0 && rc2 >= 0)
+   if (((rc1 >= 0) && (rc2 <= 0) )
+       || ((rc1 <= 0) && (rc2 >= 0)))
    {
       return 0;
    }
@@ -104,8 +105,8 @@ INT8 getYsection(PT2D* ptCorners, const CPoint& pt)
       return -1;
 
 
-   if (rc1 > 0 && rc0 > 0 
-       || rc1 < 0 && rc0 < 0)
+   if (((rc1 > 0) && (rc0 > 0)) 
+       || ((rc1 < 0) && (rc0 < 0)))
    {
       return 1;
    }
@@ -114,21 +115,24 @@ INT8 getYsection(PT2D* ptCorners, const CPoint& pt)
    return -1;
 }
 
-INT8 getYsection(PT3D* pt3Corners, const CPoint& pt)
+int getYsection(glm::vec3 pt3Corners[], float ptX, float ptY)
 {
-   PT2D pt2Corners[4];
+   glm::vec2 pt2Corners[4];
 
-   for (int i=0; i<4; i++) 
-      pt2Corners[i] = pt3Corners[i];
+   for (int i=0; i<4; i++) {
+		pt2Corners[i].x = pt3Corners[i].x;
+		pt2Corners[i].y = pt3Corners[i].y;
+   }
 
-   return getYsection(pt2Corners, pt);
+
+   return getYsection(pt2Corners, ptX, ptY);
 }
 
 
-INT8 getXsection(PT2D* ptCorners, const CPoint& pt)
+int getXsection(glm::vec2 ptCorners[], float ptX, float ptY)
 {
    int rc0, rc1, rc2;
-   PT2D ptLineStart, ptLineEnd;
+   glm::vec2 ptLineStart, ptLineEnd;
    double dx, dy;
 
    dx = (ptCorners[1].x - ptCorners[0].x)/3;
@@ -138,22 +142,22 @@ INT8 getXsection(PT2D* ptCorners, const CPoint& pt)
    ptLineStart.y = ptCorners[0].y;
    ptLineEnd.x   = ptCorners[3].x;
    ptLineEnd.y   = ptCorners[3].y;
-   rc0 = lineTest(ptLineStart, ptLineEnd, pt);
+   rc0 = lineTest(ptLineStart, ptLineEnd, ptX, ptY);
    
    ptLineStart.x += dx;
    ptLineStart.y += dy;
    ptLineEnd.x   += dx;
    ptLineEnd.y   += dy;
-   rc1 = lineTest(ptLineStart, ptLineEnd, pt);
+   rc1 = lineTest(ptLineStart, ptLineEnd, ptX, ptY);
 
    ptLineStart.x += dx;
    ptLineStart.y += dy;
    ptLineEnd.x   += dx;
    ptLineEnd.y   += dy;
-   rc2 = lineTest(ptLineStart, ptLineEnd, pt);
+   rc2 = lineTest(ptLineStart, ptLineEnd, ptX, ptY);
 
-   if (rc1 >= 0 && rc2 <= 0 
-       || rc1 <= 0 && rc2 >= 0)
+   if (((rc1 >= 0) && (rc2 <= 0)) 
+       || ((rc1 <= 0) && (rc2 >= 0)))
    {
       return 0;
    }
@@ -163,8 +167,8 @@ INT8 getXsection(PT2D* ptCorners, const CPoint& pt)
       return -1;
 
 
-   if (rc1 > 0 && rc0 > 0 
-       || rc1 < 0 && rc0 < 0)
+   if (((rc1 > 0) && (rc0 > 0)) 
+       || ((rc1 < 0) && (rc0 < 0)))
    {
       return 1;
    }
@@ -174,13 +178,15 @@ INT8 getXsection(PT2D* ptCorners, const CPoint& pt)
 }
 
 
-INT8 getXsection(PT3D* pt3Corners, const CPoint& pt)
+int getXsection(glm::vec3 pt3Corners[], float ptX, float ptY)
 {
-   PT2D pt2Corners[4];
+   glm::vec2 pt2Corners[4];
 
-   for (int i=0; i<4; i++) 
-      pt2Corners[i] = pt3Corners[i];
+   for (int i=0; i<4; i++) {
+		pt2Corners[i].x = pt3Corners[i].y;
+		pt2Corners[i].x = pt3Corners[i].y;
+   }
 
-   return getXsection(pt2Corners, pt);
+   return getXsection(pt2Corners, ptX, ptY);
 }
 
